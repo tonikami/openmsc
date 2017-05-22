1596ea114695550668312eab8ffc209de7f1cf08
AdibPD <apournazari@yahoo.co.uk>
2017-05-22 04:57:19 +0100

    ui for music

--- routes/music.js
+++ routes/music.js
@@ -2,10 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-          angular.module('gridListDemo1', ['ngMaterial'])
-.controller('AppCtrl', function($scope) {});
-  //res.render('music');
+router.get('/', function(req, res, next) {    
+  res.render('music');
 });
 
 module.exports = router;
