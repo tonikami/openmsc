66028a0bfd3c1b965b5c14a2b8ec90d50f2be975
Tunde Alao <ta2g14@soton.ac.uk>
2017-06-12 05:43:44 +0100

    Added login

--- public/library/daw/gs/events/keyboard.js
+++ public/library/daw/gs/events/keyboard.js
@@ -6,18 +6,18 @@ keyboardRouter(
 	{ fn: keyToolCAS, when: "down,up", keys: [ "alt" ],   name: "hand" },
 	{ fn: keyToolCAS, when: "down,up", keys: [ "ctrl" ],  name: "zoom" },
 	{ fn: keyToolCAS, when: "down,up", keys: [ "shift" ], name: "select" },
-	{ fn: keyTool, keys: [ "z" ], name: "zoom" },
-	{ fn: keyTool, keys: [ "s" ], name: "slip" },
-	{ fn: keyTool, keys: [ "d" ], name: "delete" },
-	{ fn: keyTool, keys: [ "c" ], name: "cut" },
-	{ fn: keyTool, keys: [ "v" ], name: "select" },
-	{ fn: keyTool, keys: [ "b" ], name: "paint" },
-	{ fn: keyTool, keys: [ "h" ], name: "hand" },
+//	{ fn: keyTool, keys: [ "z" ], name: "zoom" },
+//	{ fn: keyTool, keys: [ "s" ], name: "slip" },
+//	{ fn: keyTool, keys: [ "d" ], name: "delete" },
+//	{ fn: keyTool, keys: [ "c" ], name: "cut" },
+//	{ fn: keyTool, keys: [ "v" ], name: "select" },
+//	{ fn: keyTool, keys: [ "b" ], name: "paint" },
+//	{ fn: keyTool, keys: [ "h" ], name: "hand" },
 
 	{ fn: gs.playStop,                keys: [ " " ] },
 	{ fn: gs.playPause,               keys: [ "ctrl", " " ] },
 	{ fn: gs.samples.selected.delete, keys: [ "delete" ] },
-	{ fn: ui.btnMagnet.toggle,        keys: [ "g" ] },
+//	{ fn: ui.btnMagnet.toggle,        keys: [ "g" ] },
 	{ fn: gs.samples.selected.copy,   keys: [ "ctrl", "c" ] },
 	{ fn: gs.samples.selected.paste,  keys: [ "ctrl", "v" ] },
 	{ fn: gs.history.undo,            keys: [ "ctrl", "z" ] },
@@ -27,6 +27,12 @@ keyboardRouter(
 
 var oldTool;
 
+function checkKeyboardDisabled(callback) {
+    if (true) {
+        callback();
+    }
+}
+    
 function keyTool( route ) {
 	oldTool = ui.currentTool;
 	ui.selectTool( route.name );
--- public/library/daw/init.js
+++ public/library/daw/init.js
@@ -1,32 +1,50 @@
 "use strict";
 
-( function() {
+(function () {
+    gs.wctx = new gswaContext();
+    gs.ctx = gs.wctx.ctx;
+    gs.analyserNode = gs.wctx.ctx.createAnalyser();
+    gs.analyserNode.fftSize = 256;
+    gs.analyserData = new Uint8Array(gs.analyserNode.frequencyBinCount),
+        gs.composition = gs.wctx.createComposition();
+    gs.wctx.filters.pushBack(gs.analyserNode);
 
-gs.wctx = new gswaContext();
-gs.ctx = gs.wctx.ctx;
-gs.analyserNode = gs.wctx.ctx.createAnalyser();
-gs.analyserNode.fftSize = 256;
-gs.analyserData = new Uint8Array( gs.analyserNode.frequencyBinCount ),
-gs.composition = gs.wctx.createComposition();
-gs.wctx.filters.pushBack( gs.analyserNode );
+    ui.resize();
+    ui.setFilesWidth(200);
+    ui.setTrackLinesLeft(0);
+    ui.setTrackNamesWidth(125);
+    ui.setGridZoom(1.5, 0);
+    ui.visual.on();
+    ui.btnMagnet.toggle(true);
+    ui.tracksBg.update();
+    ui.timelineLoop.toggle(false);
 
-ui.resize();
-ui.setFilesWidth( 200 );
-ui.setTrackLinesLeft( 0 );
-ui.setTrackNamesWidth( 125 );
-ui.setGridZoom( 1.5, 0 );
-ui.visual.on();
-ui.btnMagnet.toggle( true );
-ui.tracksBg.update();
-ui.timelineLoop.toggle( false );
+    gs.history.reset();
+    gs.bpm(120);
+    gs.currentTime(0);
+    gs.compositions.init();
+    gs.composition.onended(gs.compositionStop);
+    ui.dom.btnFiles.click();
+    ui.dom.clockUnits.querySelector(".s").click();
+    ui.dom.menu.querySelector("[data-tool='paint']").click();
 
-gs.history.reset();
-gs.bpm( 120 );
-gs.currentTime( 0 );
-gs.compositions.init();
-gs.composition.onended( gs.compositionStop );
-ui.dom.btnFiles.click();
-ui.dom.clockUnits.querySelector( ".s" ).click();
-ui.dom.menu.querySelector( "[data-tool='paint']" ).click();
+    checkLogin();
+})();
 
-} )();
+function checkLogin() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            var accountSection = ui.dom.accountSection;
+            if (this.responseText) {
+                var jsonResponse = JSON.parse(this.responseText);
+                ui.dom.accountArea_SignedOut.remove();
+                ui.dom.loggedInUsername.textContent = jsonResponse.username + " ";
+            } else {
+                ui.dom.accountArea_SignedIn.remove();
+            }
+        }
+    }
+    xmlHttp.open("GET", '/api/user', true); // true for asynchronous 
+    xmlHttp.send();
+}
